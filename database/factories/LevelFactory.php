c8774640907551a76784aa3c8665c4dfb19249ac
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-02 14:52:45 -0500

    Creating entity,factory and migration to our model Level

---
+++ database/factories/LevelFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Level;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LevelFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Level::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
