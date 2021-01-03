403f545b586753368d5eed1f7b6d0ee1dd286c03
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-02 19:06:40 -0500

    Creating entity,factory and migration to our model Location

---
+++ database/migrations/2021_01_02_233923_create_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('profile_id')->unsigned();
+            $table->string('country');
+            $table->timestamps();
+
+            $table->foreign('profile_id')->references('id')->on('profiles')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $users = App\Models\User::get();
+    return view('welcome', ['users'=>$users]);
 });
