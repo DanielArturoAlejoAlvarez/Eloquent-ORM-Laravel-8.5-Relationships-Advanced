6fe12b51756d42af75316dfe00af19b21d39bdf3
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-03 18:34:39 -0500

    Define a polymorphic, inverse many-to-many relationship in Tag model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    public function posts() {
+        return $this->morphedByMany(Post::class, 'taggable');
+    }
+
+    public function videos() {
+        return $this->morphedByMany(Video::class, 'taggable');
+    }
 }
