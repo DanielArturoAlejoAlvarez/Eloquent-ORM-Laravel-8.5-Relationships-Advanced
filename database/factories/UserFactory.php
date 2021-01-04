41c200d6e4d7ee4d756fc53cd8b8149a28304d43
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-04 12:39:38 -0500

    Modify and configure DatabaseSeeder to group,level and user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,11 @@ class UserFactory extends Factory
     {
         return [
             'level_id'          =>  $this->faker->randomElement([null,1,2,3]),
-            'name'              => $this->faker->name,
-            'email'             => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token'    => Str::random(10),
+            'name'              =>  $this->faker->name,
+            'email'             =>  $this->faker->unique()->safeEmail,
+            'email_verified_at' =>  now(),
+            'password'          =>  '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    =>  Str::random(10),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,38 @@ class DatabaseSeeder extends Seeder
         \App\Models\Level::factory()->create(['name'=>'silver']);
         \App\Models\Level::factory()->create(['name'=>'bronze']);
 
+
+        \App\Models\User::factory(5)->create()->each(function($user) {
+            $profile = $user->profile()->save(\App\Models\User::factory()->make());
+            $profile->location()->save(\App\Models\Location::factory()->make());
+
+            $user->groups()->attach($this->arrayNum(rand(1,3)));
+
+            $user->image()->save(\App\Models\Image::factory()
+                        ->make(['url'=>$this->getAvatar(['men','women'],rand(1,99))]));
+        });
+
+
+
+    }
+
+    private function arrayNum($max) {
+        $values = [];
+
+        for ($i=0; $i < $max; $i++) { 
+            $values[] = $i;
+        }
+
+        return $values;
+    }
+
+    private function getPic($max) {
+        return 'https://picsum.photos/id/'.$max.'/1024/';
+    }
+
+    private function getAvatar($arr,$max) {
+        $arr_index = array_rand($arr);
+        $index = $arr[$arr_index];
+        return 'https://randomuser.me/api/portraits/'.$index.'/'.$max.'.jpg';
     }
 }
