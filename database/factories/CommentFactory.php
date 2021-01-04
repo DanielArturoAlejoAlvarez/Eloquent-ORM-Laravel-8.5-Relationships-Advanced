4a98c10061c2ffe841a0aebed83b373394085dd4
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-04 10:57:32 -0500

    Configuration factory category,comment

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -22,7 +22,8 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'body'      =>  $this->faker->text,
+            'user_id'   =>  rand(1,5)
         ];
     }
 }
